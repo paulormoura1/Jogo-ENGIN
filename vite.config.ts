a07f819fb866d4cd808cb17037f463036005e3fa
paulormoura1 <paulormoura1@gmail.com>
2026-01-15 15:18:02 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,28 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    return {
+   retornar {
+  base: "/Jogo-ENGIN/",
+
+  servidor: {
+    porta: 3000,
+    hospedar: "0.0.0.0",
+  },
+
+  plugins: [reagir()],
+
+  definir: {
+    "process.env.API_KEY": JSON.stringify(ambiente.GEMINI_API_KEY),
+    "process.env.GEMINI_API_KEY": JSON.stringify(ambiente.GEMINI_API_KEY),
+  },
+
+  resolver: {
+    pseudonimo: {
+      "@": caminho.resolver(__dirname, "."),
+    },
+  },
+};
+
       server: {
         port: 3000,
         host: '0.0.0.0',
