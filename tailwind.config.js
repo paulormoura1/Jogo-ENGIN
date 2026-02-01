db3cc5949c71080959ce85ef0f257f81b2605f11
paulormoura1 <paulormoura1@gmail.com>
2026-02-01 14:14:43 -0300

    Migrate Tailwind config to CommonJS module format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./**/*.{ts,tsx}"],
+module.exports = {
+  content: [
+    "./index.html",
+    "./*.{ts,tsx}",
+    "./**/*.{ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
