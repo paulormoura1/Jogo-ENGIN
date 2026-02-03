11eee8795e14627305c22ca464f9e7279467fdbd
paulormoura1 <paulormoura1@gmail.com>
2026-02-03 13:03:42 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -415,7 +415,7 @@ const App: React.FC = () => {
                           </div>
                         </div>
                       )}
-                  </div>
+                 </>
                 </div>
               )}
             </div>
