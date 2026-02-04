694a22173384e72d177a18d9d3d6758a8d3a48e7
paulormoura1 <paulormoura1@gmail.com>
2026-02-04 13:23:18 -0300

    Governança do Conhecimento

--- src/data/egcSources.ts
+++ src/data/egcSources.ts
@@ -20,3 +20,23 @@ export interface EGCSource {
  * Conteúdo curado, auditável e evolutivo.
  */
 export const EGC_SOURCES: EGCSource[] = [];
+export const EGC_SOURCES: EGCSource[] = [
+  {
+    id: "gc-ufsc-001",
+    area: ResearchArea.GOVERNANCE_KNOWLEDGE,
+    titulo: "Governança do Conhecimento em Organizações Públicas",
+    autores: "Programa de Pós-Graduação em Engenharia e Gestão do Conhecimento (EGC/UFSC)",
+    ano: 2021,
+    instituicao: "Universidade Federal de Santa Catarina",
+    tipo: "UFSC",
+    link: "https://repositorio.ufsc.br/",
+    palavrasChave: [
+      "governança do conhecimento",
+      "auditoria do conhecimento",
+      "maturidade organizacional",
+      "decisão estratégica"
+    ],
+    observacao: "Base conceitual utilizada como referência para desafios de Governança do Conhecimento."
+  }
+];
+
