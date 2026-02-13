0eaed93955e4044feb11873ccf143d6d2b5ccec9
paulormoura1 <paulormoura1@gmail.com>
2026-02-13 17:06:34 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { GamePhase, ResearchArea, GameState, Challenge, ActionRecord }from './src/tipos';
 import { AREA_ICONS, RESEARCH_DESCRIPTIONS } from './constants';
 import { generateChallenge } from './geminiService';
-import { getSourcesByArea } from "./src/services/services/sourcesService";
+import { getSourcesByArea } from "./src/services/sourcesService";
 
 interface ExtendedActionRecord extends ActionRecord {
   references?: string[];
