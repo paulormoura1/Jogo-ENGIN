826394f2df631ede51e51ca6cb373639aabd882b
paulormoura1 <paulormoura1@gmail.com>
2026-03-03 18:12:26 -0300

    Update articleIndex.ts

--- articleIndex.ts
+++ articleIndex.ts
@@ -7,7 +7,7 @@ export type ArticleIndexItem = {
 
 export const articleIndex: ArticleIndexItem[] = [
   {
-    doi: "10.11606/issn.2318-04012018-092030",
+    doi: "10.11606/d.55.2018.tde-04012018-092030",
     // coloque aqui links reais se quiser testar:
     // driveUrl: "https://drive.google.com/file/d/XXXX/view",
     // ufscHandle: "https://repositorio.ufsc.br/handle/XXXX/XXXX",
