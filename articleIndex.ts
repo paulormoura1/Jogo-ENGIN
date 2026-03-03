c2f02f487cf6ce5e8445c8fa60c74132d3b196af
paulormoura1 <paulormoura1@gmail.com>
2026-03-03 17:58:42 -0300

    Update articleIndex.ts

--- articleIndex.ts
+++ articleIndex.ts
@@ -6,3 +6,10 @@ export type ArticleIndexItem = {
 };
 
 export const articleIndex: ArticleIndexItem[] = [];
+export const articleIndex = [
+  {
+    doi: "10.18616/pidi12",
+    driveUrl: "https://drive.google.com/file/d/SEU_FILE_ID/view",
+    ufscHandle: "https://repositorio.ufsc.br/handle/XXXX/XXXX",
+  },
+];
