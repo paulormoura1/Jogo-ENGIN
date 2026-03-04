74404012828dec3613f5d208defd3be23e062933
paulormoura1 <paulormoura1@gmail.com>
2026-03-03 21:05:20 -0300

    Update articleIndex.ts

--- articleIndex.ts
+++ articleIndex.ts
@@ -4,10 +4,10 @@ export type ArticleIndexItem = {
   driveUrl?: string;
   ufscHandle?: string;
 };
-export const articleIndex = [
+
+export const articleIndex: ArticleIndexItem[] = [
   {
     doi: "10.11606/d.55.2018.tde-04012018-092030",
-    driveUrl: "https://drive.google.com/SEU_LINK_AQUI",
-    ufscHandle: "https://repositorio.ufsc.br/handle/SEU_HANDLE_AQUI",
+    driveUrl: "https://example.com",
   },
 ];
